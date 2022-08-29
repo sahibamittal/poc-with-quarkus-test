6020ee7a0765823656260cbf44f2e0d797a258b1
Apurva <apurva.jain@gatech.edu>
2022-08-29 16:58:33 -0500

    Corrected the port for github Ananlyser Corrected the type of Key being read on the analyser From Integer to String

--- kafka-streams-github-analyzer/src/main/java/org/acme/consumer/ComponentAnalyzer.java
+++ kafka-streams-github-analyzer/src/main/java/org/acme/consumer/ComponentAnalyzer.java
@@ -14,10 +14,10 @@ public class ComponentAnalyzer {
     @Produces
     public Topology buildTopology() {
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<Integer, String> kStream = builder.stream("event-out", Consumed.with(Serdes.Integer(), Serdes.String()));
-        kStream.foreach(new ForeachAction<Integer, String>() {
+        KStream<String, String> kStream = builder.stream("event-out", Consumed.with(Serdes.String(), Serdes.String()));
+        kStream.foreach(new ForeachAction<String, String>() {
             @Override
-            public void apply(Integer eventId, String componentName) {
+            public void apply(String eventId, String componentName) {
                 System.out.println("Got component in GitHub analyzer from event Id: "+eventId+" which has component name: "+componentName);
             }
         });
